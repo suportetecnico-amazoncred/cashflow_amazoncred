9fe6952f5c3fb3db11c272070296e9b6fbaa79a9
Assistente de TI - Amazoncred <suportetecnico@amazoncred.org.br>
2026-01-16 16:12:06 -0300

    refactor: Remove unused Cloud Functions
    The Cloud Functions have been deprecated and replaced by Firestore Security Rules to keep the project on the Firebase free tier. This directory can be safely removed.

--- functions/index.js
+++
@@ -1,4 +0,0 @@
-
-// As Cloud Functions foram desativadas e substituídas por Regras de Segurança do Firestore
-// para manter o projeto no plano gratuito do Firebase.
-// Este arquivo pode ser removido, juntamente com todo o diretório 'functions'.
